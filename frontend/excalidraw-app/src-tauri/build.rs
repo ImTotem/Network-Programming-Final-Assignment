469c039872db557526131c99b152b0eefc1b3ee6
박성빈 <psb106305@gmail.com>
2025-06-22 21:22:39 +0900

    chore: platform별 빌드 설정

--- frontend/excalidraw-app/src-tauri/build.rs
+++ frontend/excalidraw-app/src-tauri/build.rs
@@ -6,9 +6,9 @@ fn main() {
   println!("cargo:rustc-link-search=native={}/build", dst.display());
   println!("cargo:rustc-link-lib=static=collab_lib");
 
-  // Cargo가 무시할 수 없는, 링커에게 직접 전달하는 인자입니다.
-  // `-l`은 "라이브러리를 링크하라"는 의미의 링커 플래그입니다.
-  println!("cargo:rustc-link-arg=-lc++");
+  if cfg!(target_os = "macos") {
+    println!("cargo:rustc-link-arg=-lc++");
+  }
 
   tauri_build::build();
 }
